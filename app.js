61419d6eb70b7dd562b883675da0bd0d23b525db
Di <d.osseyran@hotmail.com>
2022-06-23 19:04:14 +0200

    having some trouble with CSS

---
+++ app.js
@@ -0,0 +1,7 @@
+//Figure 1 --> chair
+//for the record, there are 600 muscles in a human body (do I need 1 colour for each? )
+//Main muscles colored in red, yellow,orange, blue --> Abdominals, forearms, shoulders, psoas
+// if blue.muscle = psoas ==> Great if not "Oups wrong one, let me give you a hint ;) It starts with a "p")
+// The object is the human body
+// The properties are represented by muscles
+// HumanBody {
