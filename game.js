8c91daa98ee31818ff1dcf6fbca76cad6bc5f7f2
Di <d.osseyran@hotmail.com>
2022-07-05 18:43:48 +0200

    next

--- game.js
+++ game.js
@@ -5,3 +5,30 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+let questions = [
+  {
+    question: "Inside Which HTML element do we put the Javascript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1,
+  },
+  {
+    question: "Inside Which HTML element do we put the Javascript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1,
+  },
+];
